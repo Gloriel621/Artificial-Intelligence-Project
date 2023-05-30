37e3a186a905fe37d8a6f525b07a1af16003e262
illuminoplanet <illuminoplanet@gmail.com>
2023-05-30 22:26:57 +0900

    FEAT: Add face swapping using pretrained model

---
+++ metric/src/dataset/swap_dataset.py
@@ -0,0 +1,25 @@
+import glob
+from PIL import Image
+
+from torch.utils.data import Dataset
+
+
+class SwapDataset(Dataset):
+    def __init__(self, data_path, transforms=None, image_size=128, mask_size=64):
+        self.path = sorted(glob.glob("{}/*.jpg".format(data_path)))[-2000:]
+        self.image_size = image_size
+        self.mask_size = mask_size
+        self.transform = transforms
+        
+    def __len__(self):
+        return len(self.path)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.path[idx])
+        image = self.transform(image)
+
+        gap = (self.image_size - self.mask_size) // 2
+        image[:, gap : gap + self.mask_size, gap : gap + self.mask_size] = 1
+        image = image.unsqueeze(0)
+        
+        return image, gap
---
+++ metric/src/swapping.py
@@ -0,0 +1,48 @@
+import os 
+
+import torch
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from tqdm import tqdm
+
+from dataset import *
+from model import *
+
+
+def swap(config):
+    generator = Generator(channels=3)
+    generator.load_state_dict(config["state_dict"])
+
+    transform = transforms.Compose([
+        transforms.Resize((config["image_size"], config["image_size"])),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+    dataset = SwapDataset(
+        "metric/data/raw", 
+        transform, 
+        image_size=config["image_size"], 
+        mask_size=config["mask_size"]
+    )
+
+    generator.eval()
+    with torch.no_grad():
+        for i, (image, gap) in enumerate(tqdm(dataset)):
+            output = generator(image)
+            image[:, :, gap: gap + config["mask_size"], gap: gap + config["mask_size"]] = output
+
+            filename = dataset.path[i].split("/")[-1]
+            path = os.path.join(config["output_path"], filename)
+
+            save_image(image, path, normalize=True)
+
+
+if __name__ == "__main__":
+    
+    config = {
+        "state_dict" : torch.load("metric/save/swap_full.pth", map_location="cpu"), 
+        "image_size" : 128,
+        "mask_size" : 64,
+        "output_path" : "metric/data/swap_full",
+    }
+    swap(config)
